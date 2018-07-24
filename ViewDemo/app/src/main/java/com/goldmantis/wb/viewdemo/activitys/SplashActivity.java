77583d71dd8bb9617143f575294777cd954cbb94
Looby_H <gaoyingyong@foxmail.com>
2018-07-25 01:36:09 +0800

    201872502

---
+++ ViewDemo/app/src/main/java/com/goldmantis/wb/viewdemo/activitys/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.goldmantis.wb.viewdemo.activitys;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.goldmantis.wb.viewdemo.R;
+import com.goldmantis.wb.viewdemo.RestMainActivity;
+import com.goldmantis.wb.viewdemo.utils.uni.ThreadPoolUtils;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by Administrator on 2018/3/22.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        getWindow().getDecorView().setBackgroundResource(R.mipmap.icon_span_background);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent inRM= new Intent();
+                inRM.setClass(getContext(), RestMainActivity.class);
+                startActivity(inRM);
+                finish();
+            }
+        },3000);
+    }
+
+    private Context getContext(){
+        return this;
+    }
+}
